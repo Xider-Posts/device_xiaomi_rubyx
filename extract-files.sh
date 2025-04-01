1485f4cb250105edf2023ac54ae41c4723a66bf0
postcumer <postcumer@tuta.io>
2025-04-01 06:50:01 +0000

    rubyx: Patch `libnvram.so` and `libsysenv.so` with `libbase_shim.so`
    Signed-off-by: postcumer <postcumer@tuta.io>

--- extract-files.sh
+++ extract-files.sh
@@ -130,6 +130,10 @@ function blob_fixup {
             [ "$2" = "" ] && return 0
             grep -q "libaudioclient_shim" "${2}" || "${PATCHELF}" --add-needed "libaudioclient_shim.so" "${2}"
             ;;
+	    vendor/lib*/libnvram.so|\
+        vendor/lib64/libsysenv.so)
+            "${PATCHELF}" --add-needed "libbase_shim.so" "${2}"
+            ;;
         system_ext/lib64/libsource.so)
             [ "$2" = "" ] && return 0
             grep -q "libui_shim.so" "${2}" || "${PATCHELF}" --add-needed "libui_shim.so" "${2}"
